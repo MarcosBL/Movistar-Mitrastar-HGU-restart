1bbc071ce9d411aaa545d057042e5aff92da271a
MarcosBL <marcosbl@gmail.com>
2024-01-30 17:25:21 +0100

    Update router_restart.sh
    shebang con env
    -eou pipefail
    path a curl
    portable con || en el path de curl ?
    horrores ortográficos
    ahorrado 1 bit de memoria en el $response que no usaba
    @hluaces es mi pastor, nada me valida

--- router_restart.sh
+++ router_restart.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eou pipefail
 
 # Configuración del usuario
 router_ip='192.168.1.100'
@@ -9,23 +10,24 @@ router_user='1234'
 host="http://$router_ip"
 auth="$router_user:$router_password"
 session_file="/tmp/mitracookies.txt"
+readonly CURL_PATH="$(command -v curl || echo '/usr/bin/curl')"
 
 echo "Iniciando sesión y obteniendo cookies..."
 sessionKey=$(echo -n "$auth" | base64)
-response=$(curl -s --http0.9 -c "$session_file" -X POST "$host/login-login.cgi" --data "sessionKey=$sessionKey&pass=")
+"$CURL_PATH" -s --http0.9 -c "$session_file" -X POST "$host/login-login.cgi" --data "sessionKey=$sessionKey&pass=" > /dev/null 2>&1
 
 echo "Obteniendo formulario de reinicio..."
-result=$(curl -s --http0.9 -b "$session_file" -X GET "$host/resetrouter.html")
+result=$($CURL_PATH -s --http0.9 -b "$session_file" -X GET "$host/resetrouter.html")
 
 echo "Realizando reinicio final..."
 match=$(echo "$result" | grep -o "var sessionKey='[0-9]\+'")
 sessionKey=$(echo "$match" | grep -o "[0-9]\+")
 params="sessionKey=$sessionKey"
-result=$(curl -s --http0.9 -b "$session_file" -X GET "$host/rebootinfo.cgi?$params")
+result=$($CURL_PATH -s --http0.9 -b "$session_file" -X GET "$host/rebootinfo.cgi?$params")
 
 # Imprimir el resultado
 if [[ $result == *"The Broadband Router is rebooting"* ]]; then
     echo "✅ ¡Reiniciando!"
 else
-    echo "💥 ¡Ops... algo falló!"
+    echo "💥 Ops... ¡algo falló!"
 fi
